584f12a10b290dc0f2d92a16aeb714f3897a1041
nifemi stephen <ashasteven1@gmail.com>
2026-05-28 16:44:11 +0100

    remove from repo

--- terraform-setup/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-project-terraform-state-nifemi"
-    key            = "devops-project/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- terraform-setup/modules/ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.portfolio_server.public_ip
-}
