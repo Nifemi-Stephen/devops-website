1aa5b6c4c4d8ec49e145f2b8f14557a920a04801
nifemi stephen <ashasteven1@gmail.com>
2026-06-01 18:46:27 +0100

    deleted files

--- terraform-eks-cluster files/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your bucket name"
-    key            = "terrafrom-state-bucket folder/terraform.tfstate"
-    region         = "region"
-    dynamodb_table = "lock name"
-    encrypt        = true
-  }
-}
